4f5754ac087575bb5d59b01bcff3d17338190860
Sanjana K R <sanjanakr97@gmail.com>
2020-09-09 17:59:32 +0530

    Refator: modified jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,13 +3,10 @@ pipeline {
     stages {
         stage("deploy") {
             step{
-                ([$class: 'AWSCodeDeployPublisher',
-                     applicationName: 'chat-Application', awsAccessKey: '',
-                     awsSecretKey: '',
-                     credentials: 'awsAccessKey', deploymentConfig: 'CodeDeployDefault.OneAtATime',
-                     deploymentGroupAppspec: false, deploymentGroupName: 'demo', deploymentMethod: 'deploy',
-                     excludes: '', iamRoleArn: '', includes: '**', proxyHost: '', proxyPort: 0, region: 'us-east-2',
-                     s3bucket: 'demo24123', s3prefix: '', subdirectory: '', versionFileName: '', waitForCompletion: false])
-        }}
+                sh 'rsync -r -e "sudo ssh -i /home/ubuntu/.ssh/sanjana.pem" /var/lib/jenkins/workspace/chat-app ubuntu@190.178.0.6:/home/ubuntu/demo'
+                sh 'sudo ssh -i /home/ubuntu/.ssh/sanjana.pem ubuntu@190.178.0.6 sudo cp -r /home/ubuntu/demo/chat-app/* /new_chatapp\n'
+                sh 'sudo ssh -i /home/ubuntu/.ssh/sanjana.pem ubuntu@190.178.0.6 sudo systemctl reload nginx'
+        }
+        }
     }
 }
