39506507e80b5147e013afa4c61c994bbdc8dd88
Sanjana K R <sanjanakr97@gmail.com>
2020-09-09 17:51:57 +0530

    Refator: modified jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,14 +2,14 @@ pipeline {
     agent any
     stages {
         stage("deploy") {
-            step(
+            step {
                     [$class: 'AWSCodeDeployPublisher',
                      applicationName: 'chat-Application', awsAccessKey: '',
                      awsSecretKey: '',
                      credentials: 'awsAccessKey', deploymentConfig: 'CodeDeployDefault.OneAtATime',
                      deploymentGroupAppspec: false, deploymentGroupName: 'demo', deploymentMethod: 'deploy',
                      excludes: '', iamRoleArn: '', includes: '**', proxyHost: '', proxyPort: 0, region: 'us-east-2',
-                     s3bucket: 'demo24123', s3prefix: '', subdirectory: '', versionFileName: '', waitForCompletion: false])
-        }
+                     s3bucket: 'demo24123', s3prefix: '', subdirectory: '', versionFileName: '', waitForCompletion: false]
+        }}
     }
 }
