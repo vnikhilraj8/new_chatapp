095d2b5661f60877aef4d6dcfe1d14f57f07262c
Sanjana K R <sanjanakr97@gmail.com>
2020-09-09 17:45:23 +0530

    Added: jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    stages {
+        stage("deploy") {
+            step(
+                    [$class: 'AWSCodeDeployPublisher',
+                     applicationName: 'chat-Application', awsAccessKey: '',
+                     awsSecretKey: '',
+                     credentials: 'awsAccessKey', deploymentConfig: 'CodeDeployDefault.OneAtATime',
+                     deploymentGroupAppspec: false, deploymentGroupName: 'demo', deploymentMethod: 'deploy',
+                     excludes: '', iamRoleArn: '', includes: '**', proxyHost: '', proxyPort: 0, region: 'us-east-2',
+                     s3bucket: 'demo24123', s3prefix: '', subdirectory: '', versionFileName: '', waitForCompletion: false])
+        }
+    }
+}
