cb92427f8db49d436dce47882245006895fc24f3
Sanjana K R <sanjanakr97@gmail.com>
2020-09-09 18:00:54 +0530

    Refator: modified jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
     stages {
         stage("deploy") {
-            step{
+            steps{
                 sh 'rsync -r -e "sudo ssh -i /home/ubuntu/.ssh/sanjana.pem" /var/lib/jenkins/workspace/chat-app ubuntu@190.178.0.6:/home/ubuntu/demo'
                 sh 'sudo ssh -i /home/ubuntu/.ssh/sanjana.pem ubuntu@190.178.0.6 sudo cp -r /home/ubuntu/demo/chat-app/* /new_chatapp\n'
                 sh 'sudo ssh -i /home/ubuntu/.ssh/sanjana.pem ubuntu@190.178.0.6 sudo systemctl reload nginx'
