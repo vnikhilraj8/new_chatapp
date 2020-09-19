7a59eb06f98ae29a533720a440d31c684219977a
Sanjana K R <sanjanakr97@gmail.com>
2020-09-19 20:32:50 +0530

    Refator: modified index file and chat file

--- chat.groovy
+++ chat.groovy
@@ -19,7 +19,13 @@ node{
         withCredentials([string(credentialsId: 'docker-pwd', variable: 'dockerHubPwd')]) {
             sh "docker login -u sanjanakr -p ${dockerHubPwd}"
         }
-        def dockerRun = 'docker pull sanjanakr/my-app:latest'
-        sh "sudo ssh -i /sanjana.pem -o StrictHostKeyChecking=no ubuntu@190.178.0.11 ${dockerRun}"
+        def dockerPull = 'docker pull sanjanakr/my-app:latest'
+        def dockerImageid = 'docker image ls | awk \'$2=="<none>"{print $3}\''
+        def deleteImage = "docker rmi ${dockerImageid}"
+        def containerid = "docker container ls | grep -v -i names | awk '\$13==\"chat-app\"{print \$1}'"
+        def restartContainer = "docker restart ${containerid}"
+        sh "sudo ssh -i /sanjana.pem -o StrictHostKeyChecking=no ubuntu@190.178.0.11 ${dockerPull}"
+        sh "sudo ssh -i /sanjana.pem -o StrictHostKeyChecking=no ubuntu@190.178.0.11 ${deleteImage}"
+        sh "sudo ssh -i /sanjana.pem -o StrictHostKeyChecking=no ubuntu@190.178.0.11 ${restartContainer}"
     }
 }
