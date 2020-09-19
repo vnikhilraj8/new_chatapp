06a60b1264b90252b857a1ba1cce429a6cc8f9d0
Sanjana K R <sanjanakr97@gmail.com>
2020-09-19 19:49:39 +0530

    Added: chat jenkins file

---
+++ chat.groovy
@@ -0,0 +1,25 @@
+node{
+    stage('SCM Checkout'){
+        git url: 'https://github.com/Sanjana-cell/Django-Chat-Application.git'
+    }
+    stage('Build Docker Image'){
+        sh "docker build -t sanjanakr/my-app:${BUILD_ID}"
+    }
+
+    stage('Push Docker Image'){
+        withCredentials([string(credentialsId: 'docker-pwd', variable: 'dockerHubPwd')]) {
+            sh "docker login -u sanjanakr -p ${dockerHubPwd}"
+        }
+        sh "docker push sanjanakr/my-app:${BUILD_ID}"
+        sh "docker tag sanjanakr/my-app:${BUILD_ID}  sanjanakr/my-app:latest"
+        sh "docker push sanjanakr/my-app:latest"
+    }
+
+    stage('Run Container on Dev Server'){
+        withCredentials([string(credentialsId: 'docker-pwd', variable: 'dockerHubPwd')]) {
+            sh "docker login -u sanjanakr -p ${dockerHubPwd}"
+        }
+        def dockerRun = 'docker pull sanjanakr/my-app:latest'
+        sh "sudo ssh -i /sanjana.pem -o StrictHostKeyChecking=no ubuntu@190.178.0.11 ${dockerRun}"
+    }
+}
