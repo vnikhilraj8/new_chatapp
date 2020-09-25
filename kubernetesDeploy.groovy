d65f7c3e4bcb9adfb2392cf1137824dcd96016b2
Sanjana K R <sanjanakr97@gmail.com>
2020-09-25 22:58:47 +0530

    Refator:modified

--- kubernetesDeploy.groovy
+++ kubernetesDeploy.groovy
@@ -13,7 +13,7 @@ node{
     }
 
     stage('Deploy the pod'){
-        sh "ssh -i /home/ubuntu/.ssh/id_rsa ubuntu@18.191.122.152 kubectl delete -f /kubernetes/deploy.yml"
-        sh "ssh -i /home/ubuntu/.ssh/id_rsa ubuntu@18.191.122.152 kubectl apply -f /kubernetes/deploy.yml"
+        sh "sudo ssh -i /sanjana.pem -o StrictHostKeyChecking=no ubuntu@18.191.122.152 kubectl delete -f /kubernetes/deploy.yml"
+        sh "sudo ssh -i /sanjana.pem -o StrictHostKeyChecking=no ubuntu@18.191.122.152 kubectl apply -f /kubernetes/deploy.yml"
     }
 }
