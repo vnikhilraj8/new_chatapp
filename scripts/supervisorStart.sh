1eaee269365f5977ee0114118db372aa732f0153
Sanjana-cell <55678866+Sanjana-cell@users.noreply.github.com>
2021-05-20 17:18:47 +0530

    Update supervisorStart.sh

--- scripts/supervisorStart.sh
+++ scripts/supervisorStart.sh
@@ -1,4 +1,1 @@
-sudo cp /demo/fundoo/fundoo/settings.py /new_chatapp/fundoo/fundoo/settings.py
-sudo supervisorctl stop all
-sudo supervisorctl start all
-sudo supervisorctl update
+sudo systemctl start gunicorn.service
